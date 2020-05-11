cc45d3facece0acff992b188d2a67a36ff08e2cb
khan9920 <shafkhanmohamed23@gmail.com>
2020-05-11 23:32:27 +0530

    CORS added

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ app.use(bodyParser.json());
 //combining api URL with the uploads file
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+
+    next();
+});
+
 // combining api URL with the route to work
 app.use(apiURL + 'users', userRoute);
 app.use(apiURL + 'category', categoryRoute);
@@ -29,5 +38,3 @@ app.use(apiURL + 'payment', paymentRoute);
 app.use(apiURL + 'products/', productRoute);
 
 module.exports = app;
-
-//https://localhost:3000/api/url/category/:id
